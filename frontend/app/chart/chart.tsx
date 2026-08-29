d63a4d3f002dbd94fa30b6980a80f324d3d11d50
Max Paige <paige.m89@gmail.com>
2026-08-29 17:02:28 -0400

    table of data

--- frontend/app/chart/chart.tsx
+++ frontend/app/chart/chart.tsx
@@ -28,6 +28,57 @@ async function getSimulationData() {
     return data as ChartResponse;
 }
 
+interface TableRowProps {
+    text: string
+}
+
+function RowHeader( { text } : TableRowProps) {
+    return (
+        <th scope="row" className="w-24 h-16 border border-grey-300">
+            {text}
+        </th>
+    )
+}
+
+interface TableNumberCell {
+    value : number
+}
+
+function NumberCell( { value } : TableNumberCell) {
+    return (
+        <td className="w-20 h-16 border border-grey-300 text-center align-middle">
+            {value}
+        </td>
+    )
+}
+
+function HitsCell( { NaturalHits, SustainedHits, AutoWounds, HitNaturalOnes }: HitsResponse) {
+    return (
+        <div className="grid place-content-center">
+            <table className="table-fixed">
+                <tbody>
+                    <tr>
+                        <RowHeader text="Natural Hits" />
+                        <NumberCell value={NaturalHits} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Sustained Hits" />
+                        <NumberCell value={SustainedHits} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Auto Wounds" />
+                        <NumberCell value={AutoWounds} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Natural Ones" />
+                        <NumberCell value={HitNaturalOnes} />
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
 function DataTable() {
     const queryClient = useQueryClient()
 
@@ -40,14 +91,49 @@ function DataTable() {
     if(!data) return 'Error fetching data'
 
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <th scope="row">Attacks</th>
-                    <td>{data.AttackCount}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className="grid place-content-center">
+            <table className="table-fixed">
+                <tbody>
+                    <tr>
+                        <RowHeader text="Attacks" />
+                        <NumberCell value={data.AttackCount} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Hits" />
+                        <HitsCell 
+                            NaturalHits={data.Hits.NaturalHits}
+                            SustainedHits={data.Hits.SustainedHits}
+                            AutoWounds={data.Hits.AutoWounds}
+                            HitNaturalOnes={data.Hits.HitNaturalOnes}
+                        />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Devastating Wounds" />
+                        <NumberCell value={data.DevastatingWounds} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Regular Wounds" />
+                        <NumberCell value={data.RegularWounds} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Unsaved Wounds" />
+                        <NumberCell value={data.UnsavedWoundCount} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Mortal Wounds" />
+                        <NumberCell value={data.MortalWounds} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Damage Total" />
+                        <NumberCell value={data.DamageTotal} />
+                    </tr>
+                    <tr>
+                        <RowHeader text="Models Destroyed" />
+                        <NumberCell value={data.ModelsDestroyed} />
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
@@ -57,4 +143,4 @@ export function ChartRoot() {
             <DataTable />
         </QueryClientProvider>
     );
-}
+} 
