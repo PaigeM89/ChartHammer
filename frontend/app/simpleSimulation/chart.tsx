6e86282b0c985494c8b74dcbeb29c215b27dc0cb
Max Paige <paige.m89@gmail.com>
2026-08-30 19:30:55 -0400

    minor cleanup

--- frontend/app/simpleSimulation/chart.tsx
+++ frontend/app/simpleSimulation/chart.tsx
@@ -82,7 +82,6 @@ export function SimResultsBarChart( { simResults, isLoading=false } : ChartProps
                 <div style={{
                 position: 'absolute',
                 top: 0, left: 0, right: 0, bottom: 0,
-                //backgroundColor: 'rgba(255, 255, 255, 0.7)',
                 display: 'flex', justifyContent: 'center', alignItems: 'center',
                 zIndex: 10
                 }}>
@@ -97,7 +96,7 @@ export function SimResultsBarChart( { simResults, isLoading=false } : ChartProps
     )
 }
 
-export function SimResultsLineChart( {simResults, showVariance = true } : ChartProps ) {
+export function SimResultsLineChart( {simResults } : ChartProps ) {
     const data = transformSimResults(simResults);
     return (
         <div className="flex justify-center items-center w-full">
